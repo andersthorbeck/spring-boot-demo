b0decb3522bfb6ea87b455c506320f7da1e86450
Anders Rabo Thorbeck <andtho@knowit.no>
2018-02-16 23:29:25 +0100

    Stores forecast XML for future use

--- src/main/java/no/knowit/chapters/jvm/springbootdemo/client/WeatherClient.java
+++ src/main/java/no/knowit/chapters/jvm/springbootdemo/client/WeatherClient.java
@@ -1,6 +1,8 @@
 package no.knowit.chapters.jvm.springbootdemo.client;
 
+import java.io.StringReader;
 import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.xml.xpath.Jaxp13XPathTemplate;
@@ -12,14 +14,23 @@ public class WeatherClient {
   private static final String FORECAST_ENDPOINT_URL =
       "https://www.yr.no/sted/Norway/Oslo/Oslo/Lakkegata/varsel.xml";
 
+  private String forecastXml = null;
+
   public int fetchForecastTemperature() {
     final String temperatureXPath = "//forecast/tabular/time[1]/temperature/@value";
     return Integer.valueOf(
-        new Jaxp13XPathTemplate().evaluateAsString(temperatureXPath, fetchXmlForecast()));
+        new Jaxp13XPathTemplate().evaluateAsString(temperatureXPath, getXmlForecast()));
   }
 
-  private Source fetchXmlForecast() {
-    return new RestTemplate().getForObject(FORECAST_ENDPOINT_URL, Source.class);
+  private Source getXmlForecast() {
+    if (forecastXml == null) {
+      forecastXml = fetchXmlForecast();
+    }
+    return new StreamSource(new StringReader(forecastXml));
+  }
+
+  private String fetchXmlForecast() {
+    return new RestTemplate().getForObject(FORECAST_ENDPOINT_URL, String.class);
   }
 
 }
