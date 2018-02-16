7ebf356b3704c84d8e0d5ecfdc55a6e04b57cea5
Anders Rabo Thorbeck <andtho@knowit.no>
2018-02-16 23:41:57 +0100

    Adds scheduled job to regularly fetch updated forecast

--- src/main/java/no/knowit/chapters/jvm/springbootdemo/client/WeatherClient.java
+++ src/main/java/no/knowit/chapters/jvm/springbootdemo/client/WeatherClient.java
@@ -3,6 +3,7 @@ package no.knowit.chapters.jvm.springbootdemo.client;
 import java.io.StringReader;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.xml.xpath.Jaxp13XPathTemplate;
@@ -13,6 +14,8 @@ public class WeatherClient {
   /** Værvarsel fra Yr, levert av NRK og Meteorologisk institutt. */
   private static final String FORECAST_ENDPOINT_URL =
       "https://www.yr.no/sted/Norway/Oslo/Oslo/Lakkegata/varsel.xml";
+  private static final int ONE_SECOND_IN_MS = 1000;
+  private static final int ONE_MINUTE_IN_MS = 60 * ONE_SECOND_IN_MS;
 
   private String forecastXml = null;
 
@@ -27,6 +30,11 @@ public class WeatherClient {
       forecastXml = fetchXmlForecast();
     }
     return new StreamSource(new StringReader(forecastXml));
+  }
+
+  @Scheduled(initialDelay = 10 * ONE_SECOND_IN_MS, fixedRate = 10 * ONE_MINUTE_IN_MS)
+  private void regularlyUpdateCachedForecast() {
+    forecastXml = fetchXmlForecast();
   }
 
   private String fetchXmlForecast() {
